7e4bed95ee5899b44ec499a94d9d38e53e865491
Alex Warner <alex@overtureweb.com>
2020-09-07 15:40:42 -0700

    Initial Branch Commit

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC<Props> = ({fields}) => {
     const [optionLists, setOptionLists] = useState();
     const [selected, setSelected] = useState(["select", "select", "select"]);
     const appliedFilters = useRef<AppFil>();
-    const filteredProductList = useRef<object[]>();
+    const filteredProductList = useRef<Product[]>();
 
     useEffect(() => {
         const getProductList = async () => {
@@ -47,16 +47,12 @@ const App: React.FC<Props> = ({fields}) => {
     useEffect(() => {
         const setOptions = () => {
             if (!productList) return;
-            const optionsEls = [...(new Set(productList.map((obj: Product) => obj.height))) as Set<number>].map((value, i): JSX.Element =>
+            const optionsEls = [...(new Set(productList.map((obj: Product) => obj[fields[0]]))) as Set<number>].map((value, i): JSX.Element =>
                 <option key={`owd${i}`}>{value}</option>);
             setOptionLists([optionsEls, [], []])
         }
         setOptions();
-    }, [productList]);
-
-    useEffect(() => {
-
-    });
+    }, [productList, fields]);
 
     const handleChange = ({target: {dataset, value}}: ChangeEvent<HTMLSelectElement>) => {
         const {selectField, selectIndex} = dataset as iDataSet;
@@ -64,21 +60,22 @@ const App: React.FC<Props> = ({fields}) => {
         const nextIndex: number = +selectIndex + 1;
 
         setSelected((prevState: string[]) => {
-            const newState: string[] = prevState.slice(nextIndex).map(() => "select");
-            newState[currentIndex] = value;
-            return newState;
+            prevState[currentIndex] = value;
+            return prevState.fill("select", nextIndex);
         });
 
         //update applied filters
         appliedFilters.current = {...appliedFilters.current, [selectField]: value}
+
         //delete applied filters from i + 1 -> end
         for (let i = nextIndex; i < fields.length; i++) {
             delete appliedFilters.current[fields[i]];
         }
-        //re-filter products
 
+        //re-filter products
         filteredProductList.current = productList.filter((product: Product): boolean =>
             Object.entries(appliedFilters.current!).every(([key, value]): boolean => product[key] === value));
+        console.log(appliedFilters.current);
         //generate new option els
 
         setOptionLists((prevState: JSX.Element[]) => {
@@ -96,7 +93,7 @@ const App: React.FC<Props> = ({fields}) => {
             <div className="form-row justify-content-center">
                 <div className="form-group col-12 col-md-3 col-lg-2">
                     <label className="mr-2" htmlFor="productHeight">Height:</label>
-                    <select value={selected[0]} onChange={handleChange} data-select-field="height" data-select-index={0}
+                    <select value={selected[0]} onChange={handleChange} data-select-field={fields[0]} data-select-index={0}
                             id="productHeight"
                             className="form-control selectPet">
                         <option>select</option>
@@ -104,7 +101,7 @@ const App: React.FC<Props> = ({fields}) => {
                 </div>
                 <div className="form-group col-12 col-md-3 col-lg-2">
                     <label className="mr-2" htmlFor="productWidth">Width:</label>
-                    <select value={selected[1]} onChange={handleChange} data-select-field="width" data-select-index={1}
+                    <select value={selected[1]} onChange={handleChange} data-select-field={fields[1]} data-select-index={1}
                             id="productWidth"
                             className="form-control selectPet">
                         <option>select</option>
@@ -112,7 +109,7 @@ const App: React.FC<Props> = ({fields}) => {
                 </div>
                 <div className="form-group col-12 col-md-3 col-lg-2">
                     <label className="mr-2" htmlFor="productDepth">Depth:</label>
-                    <select value={selected[2]} onChange={handleChange} data-select-field="depth" data-select-index={2}
+                    <select value={selected[2]} onChange={handleChange} data-select-field={fields[2]} data-select-index={2}
                             id="productDepth"
                             className="form-control selectPet">
                         <option>select</option>
